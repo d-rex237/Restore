4665dff93153ed50dd21c7590765ef1d33b6cd2a
d-rex237 <jmc45855@gmail.com>
2026-07-30 11:17:18 +0100

    admin changes

--- app/(admin)/provider/layout.tsx
+++ app/(admin)/provider/layout.tsx
@@ -1,15 +1,16 @@
 import NavLinks from "@/components/navLink";
 
-export default function ProviderLayout({
+export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen">
-      <aside className="w-64 shrink-0 border-r border-border bg-surface px-4 py-6">
-        <div className="mb-8 px-2">
-          <h2 className="font-display text-lg font-bold">Restor Provider</h2>
+    <div className="flex min-h-screen ">
+      <aside className="w-64 shrink-0 sticky top-0 h-screen overflow-y-auto border-r border-border bg-[#f5f5f5] px-4 py-6">
+        <div className="mb-8 px-2 ">
+          <h2 className="text-2xl font-serif text-primary">Restore Admin</h2>
+          <p>Manage Users And Drivers</p>
         </div>
         <NavLinks userType="provider" />
       </aside>
