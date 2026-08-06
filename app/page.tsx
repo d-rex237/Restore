4bdee595181a01b54cb2b40674a7dbf0c8598d32
d-rex237 <jmc45855@gmail.com>
2026-08-06 09:57:44 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/app/(landind)/home/components/layout/hero";
 import FeaturedSection from "@/app/(landind)/home/components/layout/FeaturedSection";
 
-
 export default function Home() {
   return (
     <main className="flex-1">
