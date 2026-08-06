77604c0e7b89d26ecc6eeaef9f2657276d417bf5
telrahbrandly682 <telrahbrandly68230@gmail.com>
2026-08-06 09:51:36 +0100

    app

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Hero from "@/app/(landind)/home/components/layout/hero";
 import FeaturedSection from "@/app/(landind)/home/components/layout/FeaturedSection";
-
+import Footer from "@/app/(landind)/home/components/layout/Footer";
+import Navbar from "@/app/(landind)/home/components/layout/Navbar";
 
 export default function Home() {
   return (
     <main className="flex-1">
+      <Navbar />
       <Hero />
       <FeaturedSection />
+      <Footer />
     </main>
   );
 }
