0dd26fcc616a55bf8183fd22128d8d1d27de316e
telrahbrandly682 <telrahbrandly68230@gmail.com>
2026-08-06 09:39:44 +0100

    app

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/app/(landind)/home/components/layout/hero";
+import FeaturedSection from "@/app/(landind)/home/components/layout/FeaturedSection";
+
+
+export default function Home() {
+  return (
+    <main className="flex-1">
+      <Hero />
+      <FeaturedSection />
+    </main>
+  );
+}
